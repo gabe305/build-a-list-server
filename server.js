7bc86927b3cc9c6ae4a32121839acce94b4beae7
gabe305 <gabecruz718@gmail.com>
2022-06-30 23:57:05 -0400

    refresh token functional

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken
+    })
+
+    spotifyApi.refreshAccessToken()
+        .then((data) => {
+            res.json({
+                accessToken: data.body.access_token,
+                expiresIn: data.body.expires_in
+            })
+        })
+        .catch(err => {
+        })
+})
+
 app.post('/login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
@@ -22,7 +42,6 @@ app.post('/login', (req, res) => {
             expiresIn: data.body.expires_in,
         })
     }).catch(err => {
-        console.log(err)
     })
 })
 
